1a75cda6e3c7c837f222a85a6322aaf990d90ff9
Bob Carpenter <bcarp1026@gmail.com>
2021-12-03 03:46:49 +0000

    installed yarn (hopefully)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,6 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
+
 
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
 // vendor/assets/javascripts directory can be referenced here using a relative path.
@@ -16,4 +14,8 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require_tree .
+//= require_tree 
+
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
